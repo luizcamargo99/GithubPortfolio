bfb61575486dd8525871e41b1ee0fe25a066c5ed
Luiz Camargo <luiz.zito@hotmail.com>
2023-01-07 13:28:54 +0000

    Add Home Page

---
+++ src/GithubPortfolio.Web/Models/InputModel.cs
@@ -0,0 +1,10 @@
+﻿namespace GithubPortfolio.Web.Models;
+
+public class InputModel
+{
+    public required string Id { get; set; }
+    public string Value { get; set; }
+    public string HtmlClass { get; set; }
+    public required string Name { get; set; }
+    public required bool IsMandatory { get; set; }
+}
