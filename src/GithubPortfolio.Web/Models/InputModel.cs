ff3b1ea0cda086416a9a42bda0b52e91a2d78398
Luiz Camargo <luiz.zito@hotmail.com>
2023-01-07 13:45:55 +0000

    Updates in InputModel

--- src/GithubPortfolio.Web/Models/InputModel.cs
+++ src/GithubPortfolio.Web/Models/InputModel.cs
@@ -3,8 +3,8 @@
 public class InputModel
 {
     public required string Id { get; set; }
-    public string Value { get; set; }
-    public string HtmlClass { get; set; }
+    public string Value { get; set; } = null!;
+    public string HtmlClass { get; set; } = string.Empty;
     public required string Name { get; set; }
     public required bool IsMandatory { get; set; }
 }
